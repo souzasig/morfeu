9289cf16b9fbcf52871a012224508a19fc593708
souzasig <65099443+souzasig@users.noreply.github.com>
2020-05-11 19:37:58 -0300

    Update nil.js

--- nil.js
+++ nil.js
@@ -1,1 +1,12 @@
+ function pasuser(form) {
+if (form.id.value=="sousa"||form.id.value=="reinaldo"||form.id.value=="rita"){ 
+if(form.pass.value=="123"){              
 
+
+location="https://player.openload.network/qweowqie.php?4=JCrtL6SyNaltLhQTLqZpgzn74fni4ReGDhntNRdyXhPPL7OyLRP7DfQyLaXEDKvy4RpqgFdiXf8Tb9woL5ZTL6A=&3=JCrtL6SyNaltLhQTLqZpgzn74fni4ReGDhntNRdyXhPPL7OyLRP7DfQyLaXEDKvy4RpqgFdiXf8Tl98oL5ZTL6A=&2=JCrtL6SyNaltLhQTLqZpgzn74fni4ReGDhntNRdyXhPPL7OyLRP7DfQyLaXEDKvy4RpqgFdiXf8TbY3xbC3GDc3t&1=&subt=JCrtL6SyNR0GXCniDRnTJKAGDhntNRvP4RnG4KQ9No==" 
+} else {
+alert(" senha invalida")
+}
+} else {  alert("seu nome não costa em nosso sistema")
+}
+}
