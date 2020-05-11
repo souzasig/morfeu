c648e09a9c8c84c28de30bd917442e766ad62f7b
souzasig <65099443+souzasig@users.noreply.github.com>
2020-05-11 18:24:10 -0300

    Update leva.js

--- leva.js
+++ leva.js
@@ -1,1 +1,18 @@
+var login = prompt("Usuário:", "");
+var senha = prompt("Senha:", "");
 
+//substitua celiometal e rock pelo usuario e senha desejado
+//faça o mesmo da linha abaixo substituindo o usuario admim e senha 123
+//para adicionar mais usuarios é so usar o mesmo codigo e adicinar o usuario e senha
+
+if(login != "sousa" || senha != "123")
+if(login != "rita" || senha != "321")
+if(login != "reinaldo" || senha != "123")
+if(login != "arminda" || senha != "321")
+if(login != "marcos" || senha != "444")
+{
+alert("Login e/ou senha incorretos.");
+
+//crie uma pagina e salve como erro.html, coloque um link para voltar a pagina principal(opcional)
+document.location = "file:///home/souzasig/%C3%81rea%20de%20Trabalho/bia%20e%20lolo/teste.html";
+}
