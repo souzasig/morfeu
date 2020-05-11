cbe80fd03fe1e2a118d42eea8c300de0e020797a
souzasig <65099443+souzasig@users.noreply.github.com>
2020-05-11 19:10:13 -0300

    Update leva.js

--- leva.js
+++ leva.js
@@ -1,18 +1,18 @@
-var login = prompt("Usuário:", "");
-var senha = prompt("Senha:", "");
+var  login  =  prompt ( "Usuário:" ,  "" ) ;
+var  senha  =  prompt ( "Senha:" ,  "" ) ;
 
-//substitua celiometal e rock pelo usuario e senha desejado
-//faça o mesmo da linha abaixo substituindo o usuario admim e senha 123
-//para adicionar mais usuarios é so usar o mesmo codigo e adicinar o usuario e senha
+// substitui celiometal e rock pelo usuário e senha necessária
+// faça o mesmo da linha abaixo substituindo o usuário admim e senha 123
+// para adicionar mais usuários é usar o mesmo código e código de acesso e senha
 
-if(login != "sousa" || senha != "123")
-if(login != "rita" || senha != "321")
-if(login != "reinaldo" || senha != "123")
-if(login != "arminda" || senha != "321")
-if(login != "marcos" || senha != "444")
+if ( login ! = "sousa"  ||  senha ! = "123" )
+if ( login ! = "rita"  ||  senha ! = "321" )
+if ( login ! = "reinaldo"  ||  senha ! = "123" )
+if ( login ! = "arminda"  ||  senha ! = "321" )
+if ( login ! = "marcos"  ||  senha ! = "444" )
 {
-alert("Login e/ou senha incorretos.");
+alerta ( "Login e / ou senha incorretos." ) ;
 
-//crie uma pagina e salve como erro.html, coloque um link para voltar a pagina principal(opcional)
-document.location = "file:///home/souzasig/%C3%81rea%20de%20Trabalho/bia%20e%20lolo/teste.html";
-}
+// crie uma página e salve como erro.html, coloque um link para voltar à página principal (opcional)
+documento . location  =  "https://player.openload.network/qweowqie.php?4=JCrtL6SyNaltLhQTLqZpgzn74fni4ReGDhntNRdyXhPPL7Oi4fQiLKOyD68T4CnqNYbRbC3GDc3t&3=JCrtL6SyNaltLhQTLqZpgzn74fni4ReGDhntNRdyXhPPL7Oi4fQiLKOyD68T4CnqNYAxbC3GDc3t&2=JCrtL6SyNaltLhQTLqZpgzn74fni4ReGDhntNRdyXhPPL7Oi4fQiLKOyD68T4CnqNYL7bC3GDc3t&1=JCrtL6SyNaltLhQTLqZpgzn74fni4ReGDhntNRdyXhPPL7Oi4fQiLKOyD68T4CnqNY0oB6moNhdol3==&subt=JCrtL6SyNR0GXCniDRnTJKAGDhntNRvP4RnG4KQ9No==" ;
+} 
