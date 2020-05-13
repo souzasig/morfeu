fada1edaa1597457b9e9407cda51a433b3ace3fe
souzasig <65099443+souzasig@users.noreply.github.com>
2020-05-12 22:29:47 -0300

    Update nil.js

--- nil.js
+++ nil.js
@@ -1,5 +1,5 @@
 function pasuser(form) {
-if (form.id.value=="sousa"&&form.pass.value=="123"||form.id.value=="reinaldo"&&form.pass.value=="555"||form.id.value=="rita"&&form.pass.value=="999"){ 
+if (form.id.value=="sousa"&&form.pass.value=="444"||form.id.value=="reinaldo"&&form.pass.value=="555"||form.id.value=="rita"&&form.pass.value=="999"){ 
 if(form.pass.value=="0"||"0"){              
 
 
