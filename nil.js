df3c91cf163cb5f69db44dbeac231207135a7856
souzasig <65099443+souzasig@users.noreply.github.com>
2021-04-25 07:24:51 +0000

    Update nil.js

--- nil.js
+++ nil.js
@@ -1,11 +1,12 @@
-function pasuser(form) {
-if (form.id.value=="sousa"&&form.pass.value=="555"||form.id.value=="reinaldo"&&form.pass.value=="666"||form.id.value=="rita"&&form.pass.value=="999"||form.id.value=="bruna"&&form.pass.value=="2828"){ 
-if(form.pass.value=="0"||"0"){              
+
+function  pasuser ( form )  {
+if  ( form . id . value == "sousa" && form . pass . value == "555" || form . id . value == "reinaldo" && form . pass . value == "666" || form . id . value == "rita" && form . pass . value == "999" || form .id .valor == "bruna" && formulário . passe . valor == "2828" ) { 
+if ( form . pass . value == "0" || "0" ) {              
 
 
-location="https://sites.google.com/view/rafaelinvestimento/in%C3%ADcio"
-alert(" Ola Rafa  bem vindo")
+location = "https://sites.google.com/view/rafaelinvestimento/in%C3%ADcio"
+alerta ( "Ola Rafa bem vindo" )
 }
-} else {  alert("seu nome não costa em nosso sistema")
+}  else  {   alert ( "seu nome não costa em nosso sistema" )
 }
 }
